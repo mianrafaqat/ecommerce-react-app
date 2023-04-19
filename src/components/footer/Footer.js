972fe31bf6aee8a482afef285bcd27192ed5a288
Rafaqat Ali <83506643+mianrafaqat@users.noreply.github.com>
2023-04-19 10:26:14 +0500

    footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,12 @@
 import React from "react";
+import styyles from "./footer.module.scss";
 
+const data = new Date();
+const year = data.getFullYear();
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styyles.footer}>&copy; {year} All Rights Reserved</div>
+  );
 };
 
 export default Footer;
