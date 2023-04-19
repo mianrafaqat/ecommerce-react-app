93445ba1852d3e4fb087b10c9003ebcadb0ab5e5
Rafaqat Ali <83506643+mianrafaqat@users.noreply.github.com>
2023-04-19 12:35:56 +0500

    reset

--- src/pages/auth/Reset.js
+++ src/pages/auth/Reset.js
@@ -1,7 +1,38 @@
 import React from "react";
+import resetImg from "../../assets/forget.jpg";
+import styles from "./auth.module.scss";
+import Card from "../../components/card/Card";
+import { Link } from "react-router-dom";
 
 const Reset = () => {
-  return <div>Reset</div>;
+  return (
+    <section className={`container ${styles.auth}`}>
+      <div className={styles.img}>
+        <img src={resetImg} alt="reset" width="400" />
+      </div>
+
+      <Card>
+        <div className={styles.form}>
+          <h2>Forget Password</h2>
+
+          <form>
+            <input type="text" placeholder="Email" required />
+            <button className="--btn --btn-primary --btn-block">
+              Reset Password
+            </button>
+            <div className={styles.links}>
+              <p>
+                <Link to="/login">- Login</Link>
+              </p>
+              <p>
+                <Link to="/register">- Register</Link>
+              </p>
+            </div>
+          </form>
+        </div>
+      </Card>
+    </section>
+  );
 };
 
 export default Reset;
